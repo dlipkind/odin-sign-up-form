86b38ef02011e49e142df85b33c11d61fbc5cb82
Daniel <danielipkind@gmail.com>
2022-12-11 19:42:22 +0200

    Add basic CSS

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form');
+const button = document.getElementsByTagName('button');
 const error = document.getElementById('error');
 const first_name = document.getElementsByName('first_name');
 const last_name = document.getElementsByName('last_name');
@@ -6,3 +7,7 @@ const email = document.getElementsByName('email');
 const phone_number = document.getElementsByName('phone_number');
 const password = document.getElementsByName('password');
 const password_conf = document.getElementsByName('password_conf');
+
+
+button.addEventListener("submit", myFunction);
+
